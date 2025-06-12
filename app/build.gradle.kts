0485025769ac2fbbe55111d3d2ed8c7b870a354b
jezierski999 <jezierski999@gmail.com>
2025-06-12 21:58:47 +0200

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+import com.android.build.api.dsl.AaptOptions
+import com.android.build.api.dsl.AndroidResources
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.speedlimitdetector"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.speedlimitdetector"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    fun AndroidResources.() {
+        noCompress += "tflite" // noCompress += "lite" // jeśli też chcesz
+    } // noCompress += "lite" // jeśli też chcesz
+    androidResources
+
+    fun Packaging.() {
+        resources {
+            excludes += setOf("META-INF/INDEX.LIST", "META-INF/DEPENDENCIES")
+        }
+    }
+
+    buildFeatures {
+        viewBinding=true
+    }
+
+    sourceSets {
+        getByName("main") {
+            mlModels.srcDir("src/main/ml")
+        }
+    }
+    buildFeatures {
+        mlModelBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("com.google.mlkit:image-labeling-custom-common:17.0.0")
+
+    implementation ("androidx.camera:camera-core:1.4.2")
+    implementation ("androidx.camera:camera-camera2:1.4.2")
+    implementation ("androidx.camera:camera-lifecycle:1.4.2")
+    implementation ("com.google.mlkit:image-labeling-custom-common:17.0.0")
+
+    implementation ("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation ("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation ("com.google.mlkit:image-labeling-custom:17.0.1")
+    implementation ("com.google.mlkit:translate:17.0.1")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation ("com.google.mlkit:text-recognition:16.0.0")
+}
